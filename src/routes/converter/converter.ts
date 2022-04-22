e450a488036e55e0c13d7109d4630a8ffc1f91c3
Ramzy98 <ahmadramzy988@gmail.com>
2022-04-22 17:30:29 +0200

    removed useless await

--- src/routes/converter/converter.ts
+++ src/routes/converter/converter.ts
@@ -12,7 +12,7 @@ async function resizeImage(filename: string, width: number, height: number) {
   }
   const inputImage = `./images/full/${filename}.jpg`;
   const outputImagePath = `./images/thumb/${filename}-thumb.jpg`;
-  const outputImage = await sharp(inputImage).resize(width, height);
+  const outputImage = sharp(inputImage).resize(width, height);
   await outputImage.toFile(outputImagePath);
   return outputImagePath;
 }
